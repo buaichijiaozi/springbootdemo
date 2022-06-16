4afa0d1dec5d0df8470d8009037f4891b8e34c9f
dongzhi <dongzhifun@outlook.com>
2022-06-17 02:59:53 +0800

    inside 2 6-17

---
+++ src/main/java/com/dz/springboard/config/LoginInterceptorConfigurer.java
@@ -0,0 +1,33 @@
+package com.dz.springboard.config;
+
+import com.dz.springboard.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class LoginInterceptorConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        HandlerInterceptor loginInterceptor = new LoginInterceptor();
+        List<String> excludePath = new ArrayList<>();
+        excludePath.add("/bootstrap/**");
+        excludePath.add("/css/**");
+        excludePath.add("/js/**");
+        excludePath.add("/images/**");
+        excludePath.add("/web/login.html");
+        excludePath.add("/web/register.html");
+        excludePath.add("/web/index.html");
+        excludePath.add("/web/product.html");
+        excludePath.add("/users/reg");
+        excludePath.add("/users/login");
+
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(excludePath);
+    }
+}
